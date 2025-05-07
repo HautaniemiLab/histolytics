d1b56e14d20302f325fd1553f085e632434efc0e
Oskari <oskari.lehtonen@helsinki.fi>
2025-05-07 15:48:43 +0300

    feat: add initial panoptic segmentation models

---
+++ src/histolytics/models/cellvit_panoptic.py
@@ -0,0 +1,63 @@
+from typing import Any, Dict
+
+import torch
+from cellseg_models_pytorch.inference.post_processor import PostProcessor
+from cellseg_models_pytorch.inference.predictor import Predictor
+from cellseg_models_pytorch.models.cellvit.cellvit_unet import cellvit_panoptic
+
+from histolytics.models._base_model import BaseModelPanoptic
+
+__all__ = ["CellVitPanoptic"]
+
+
+class CellVitPanoptic(BaseModelPanoptic):
+    model_name = "cellvit_panoptic"
+
+    def __init__(
+        self,
+        n_nuc_classes: int,
+        n_tissue_classes: int,
+        enc_name: str = "efficientnet_b5",
+        enc_pretrain: bool = True,
+        enc_freeze: bool = False,
+        device: torch.device = torch.device("cuda"),
+        model_kwargs: Dict[str, Any] = {},
+    ) -> None:
+        """CellVitPanoptic model for panoptic segmentation of nuclei and tissues.
+
+        Parameters:
+            n_nuc_classes (int):
+                Number of nuclei type classes.
+            n_tissue_classes (int):
+                Number of tissue type classes.
+            enc_name (str, default="efficientnet_b5"):
+                Name of the pytorch-image-models encoder.
+            enc_pretrain (bool, default=True):
+                Whether to use pretrained weights in the encoder.
+            enc_freeze (bool, default=False):
+                Freeze encoder weights for training.
+            device (torch.device, default=torch.device("cuda")):
+                Device to run the model on. Default is "cuda".
+        """
+        super().__init__()
+        self.model = cellvit_panoptic(
+            n_nuc_classes,
+            n_tissue_classes,
+            enc_name=enc_name,
+            enc_pretrain=enc_pretrain,
+            enc_freeze=enc_freeze,
+            **model_kwargs,
+        )
+
+        self.device = device
+        self.model.to(device)
+
+    def set_inference_mode(self, mixed_precision: bool = True) -> None:
+        """Set to inference mode."""
+        self.model.eval()
+        self.predictor = Predictor(
+            model=self.model,
+            mixed_precision=mixed_precision,
+        )
+        self.post_processor = PostProcessor(postproc_method="hovernet")
+        self.inference_mode = True
---
+++ src/histolytics/models/stardist_panoptic.py
@@ -0,0 +1,70 @@
+from typing import Any, Dict
+
+import torch
+from cellseg_models_pytorch.inference.post_processor import PostProcessor
+from cellseg_models_pytorch.inference.predictor import Predictor
+from cellseg_models_pytorch.models.stardist.stardist_unet import stardist_panoptic
+
+from histolytics.models._base_model import BaseModelPanoptic
+
+__all__ = ["StarDistPanoptic"]
+
+
+class StarDistPanoptic(BaseModelPanoptic):
+    model_name = "stardist_panoptic"
+
+    def __init__(
+        self,
+        n_nuc_classes: int,
+        n_tissue_classes: int,
+        n_rays: int = 32,
+        enc_name: str = "efficientnet_b5",
+        enc_pretrain: bool = True,
+        enc_freeze: bool = False,
+        device: torch.device = torch.device("cuda"),
+        model_kwargs: Dict[str, Any] = {},
+    ) -> None:
+        """StardistPanoptic model for panoptic segmentation of nuclei and tissues.
+
+        Parameters:
+            n_nuc_classes (int):
+                Number of nuclei type classes.
+            n_tissue_classes (int):
+                Number of tissue type classes.
+            n_rays (int, default=32):
+                Number of rays for the Stardist model.
+            enc_name (str, default="efficientnet_b5"):
+                Name of the pytorch-image-models encoder.
+            enc_pretrain (bool, default=True):
+                Whether to use pretrained weights in the encoder.
+            enc_freeze (bool, default=False):
+                Freeze encoder weights for training.
+            device (torch.device, default=torch.device("cuda")):
+                Device to run the model on. Default is "cuda".
+        """
+        super().__init__()
+        self.model = stardist_panoptic(
+            n_rays=n_rays,
+            n_nuc_classes=n_nuc_classes,
+            n_tissue_classes=n_tissue_classes,
+            enc_name=enc_name,
+            enc_pretrain=enc_pretrain,
+            enc_freeze=enc_freeze,
+            **model_kwargs,
+        )
+
+        self.device = device
+        self.model.to(device)
+
+    def set_inference_mode(self, mixed_precision: bool = True) -> None:
+        """Set to inference mode."""
+        self.model.eval()
+        self.predictor = Predictor(
+            model=self.model,
+            mixed_precision=mixed_precision,
+        )
+        self.post_processor = PostProcessor(
+            postproc_method="stardist",
+            postproc_kwargs={"trim_bboxes": True},
+        )
+        self.inference_mode = True
