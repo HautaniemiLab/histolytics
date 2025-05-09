c73211b648fdb74591589e18e64ac8b04a924c78
Oskari <oskari.lehtonen@helsinki.fi>
2025-05-09 10:08:24 +0300

    tests: adjust tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,11 +2,11 @@ import numpy as np
 import pytest
 import torch
 
-from histolytics.models.cellpose_panoptic import CellposePanoptic
-from histolytics.models.cellvit_panoptic import CellVitPanoptic
-from histolytics.models.cppnet_panoptic import CPPNetPanoptic
-from histolytics.models.hovernet_panoptic import HoverNetPanoptic
-from histolytics.models.stardist_panoptic import StarDistPanoptic
+from histolytics.models.cellpose_panoptic import CellposePanoptic, cellpose_panoptic
+from histolytics.models.cellvit_panoptic import CellVitPanoptic, cellvit_panoptic
+from histolytics.models.cppnet_panoptic import CPPNetPanoptic, cppnet_panoptic
+from histolytics.models.hovernet_panoptic import HoverNetPanoptic, hovernet_panoptic
+from histolytics.models.stardist_panoptic import StarDistPanoptic, stardist_panoptic
 
 
 @pytest.mark.parametrize(
@@ -55,3 +55,79 @@ def test_model_inference_torch(model):
     assert isinstance(output, dict)
     assert "nuc" in output
     assert "tissue" in output
+
+
+@pytest.mark.parametrize("enc_name", ["resnet18", "samvit_base_patch16"])
+def test_cppnet_fwdbwd(enc_name):
+    n_rays = 3
+    x = torch.rand([1, 3, 64, 64])
+    model = cppnet_panoptic(n_rays, 3, 3, enc_name=enc_name)
+
+    y = model(x)
+    y["nuc"].aux_map.mean().backward()
+
+    assert y["nuc"].type_map.shape == x.shape
+    assert y["nuc"].aux_map.shape == torch.Size([1, 3, 64, 64])
+    assert y["tissue"].type_map.shape == torch.Size([1, 3, 64, 64])
+
+
+@pytest.mark.parametrize(
+    "enc_name",
+    [
+        "samvit_base_patch16",
+        "samvit_base_patch16_224",
+        "samvit_huge_patch16",
+        "samvit_large_patch16",
+    ],
+)
+def test_cellvit_fwdbwd(enc_name):
+    x = torch.rand([1, 3, 64, 64])
+    model = cellvit_panoptic(enc_name, 3, 3, enc_pretrain=False)
+
+    y = model(x)
+    y["nuc"].aux_map.mean().backward()
+
+    assert y["nuc"].type_map.shape == x.shape
+    assert y["nuc"].aux_map.shape == torch.Size([1, 2, 64, 64])
+    assert y["tissue"].type_map.shape == torch.Size([1, 3, 64, 64])
+
+
+@pytest.mark.parametrize("enc_name", ["resnet18", "samvit_base_patch16"])
+def test_hovernet_fwdbwd(enc_name):
+    x = torch.rand([1, 3, 64, 64])
+    model = hovernet_panoptic(3, 3, enc_name=enc_name)
+
+    y = model(x)
+    y["nuc"].aux_map.mean().backward()
+
+    assert y["nuc"].type_map.shape == x.shape
+    assert y["nuc"].aux_map.shape == torch.Size([1, 2, 64, 64])
+
+    assert y["tissue"].type_map.shape == torch.Size([1, 3, 64, 64])
+
+
+@pytest.mark.parametrize("enc_name", ["resnet18", "samvit_base_patch16"])
+def test_stardist_fwdbwd(enc_name):
+    n_rays = 3
+    x = torch.rand([1, 3, 64, 64])
+    model = stardist_panoptic(n_rays, 3, 3, enc_name=enc_name)
+
+    y = model(x)
+    y["nuc"].aux_map.mean().backward()
+
+    assert y["nuc"].type_map.shape == x.shape
+    assert y["nuc"].aux_map.shape == torch.Size([1, 3, 64, 64])
+    assert y["tissue"].type_map.shape == torch.Size([1, 3, 64, 64])
+
+
+@pytest.mark.parametrize("enc_name", ["resnet18", "samvit_base_patch16"])
+def test_cellpose_fwdbwd(enc_name):
+    x = torch.rand([1, 3, 64, 64])
+    model = cellpose_panoptic(3, 3, enc_name=enc_name)
+
+    y = model(x)
+    y["nuc"].aux_map.mean().backward()
+
+    assert y["nuc"].type_map.shape == x.shape
+    assert y["nuc"].aux_map.shape == torch.Size([1, 2, 64, 64])
+    assert y["tissue"].type_map.shape == torch.Size([1, 3, 64, 64])
