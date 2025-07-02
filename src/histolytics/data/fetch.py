c938240589ee2881835db84c176afb5363f1d8c7
Oskari <oskari.lehtonen@helsinki.fi>
2025-07-02 19:26:50 +0300

    feat: add raster example data

--- src/histolytics/data/fetch.py
+++ src/histolytics/data/fetch.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import geopandas as gpd
+import numpy as np
 
 from histolytics.utils import FileHandler
 
@@ -12,6 +13,8 @@ __all__ = [
     "cervix_tissue_crop",
     "cervix_nuclei_crop",
     "hgsc_cancer_nuclei",
+    "hgsc_cancer_inst_mask",
+    "hgsc_cancer_type_mask",
     "hgsc_cancer_he",
     "hgsc_tissue_wsi",
     "hgsc_nuclei_wsi",
@@ -37,6 +40,9 @@ def _load(f):
 def cervix_tissue():
     """A GeoDataframe of segmented tissue regions of a cervical biopsy.
 
+    Note:
+        Pairs with: `cervix_nuclei()`.
+
     Examples:
         >>> from histolytics.data import cervix_tissue
         >>> ax = cervix_tissue().plot(column="class_name")
@@ -49,6 +55,8 @@ def cervix_tissue():
 def cervix_nuclei():
     """A GeoDataframe of segmented nuclei of a cervical biopsy.
 
+    Note:
+        Pairs with: `cervix_tissue()`.
 
     Examples:
         >>> from histolytics.data import cervix_nuclei
@@ -62,6 +70,9 @@ def cervix_nuclei():
 def hgsc_tissue_wsi():
     """A GeoDataframe of segmented tissue regions of a HGSC WSI.
 
+    Note:
+        Pairs with: `hgsc_nuclei_wsi()`.
+
     Examples:
         >>> from histolytics.data import hgsc_tissue_wsi
         >>> ax = hgsc_tissue_wsi().plot(column="class_name")
@@ -73,6 +84,9 @@ def hgsc_tissue_wsi():
 
 def hgsc_nuclei_wsi():
     """A GeoDataframe of segmented nuclei of a HGSC WSI.
+
+    Note:
+        Pairs with: `hgsc_tissue_wsi()`.
 
     Examples:
         >>> from histolytics.data import hgsc_nuclei_wsi
@@ -86,6 +100,9 @@ def hgsc_nuclei_wsi():
 def cervix_tissue_crop():
     """A GeoDataframe of segmented tissue regions of cervical tissue crop.
 
+    Note:
+        Pairs with: `cervix_nuclei_crop()`.
+
     Examples:
         >>> from histolytics.data import cervix_tissue_crop
         >>> ax = cervix_tissue_crop().plot(column="class_name")
@@ -98,6 +115,9 @@ def cervix_tissue_crop():
 def cervix_nuclei_crop():
     """A GeoDataframe of segmented nuclei of cervical tissue crop.
 
+    Note:
+        Pairs with: `cervix_tissue_crop()`.
+
     Examples:
         >>> from histolytics.data import cervix_nuclei_crop
         >>> ax = cervix_nuclei_crop().plot(column="class_name")
@@ -108,31 +128,82 @@ def cervix_nuclei_crop():
 
 
 def hgsc_cancer_nuclei():
-    """A GeoDataframe of segmented nuclei of a HGSC tumor nest. Pairs with `hgsc_cancer_he`
+    """A GeoDataframe of segmented nuclei of a HGSC tumor nest.
+
+    Note:
+        Pairs with:
+
+        - `hgsc_cancer_he()`
+        - `hgsc_cancer_inst_mask()`
+        - `hgsc_cancer_type_mask()`
 
     Examples:
-        >>> from histolytics.data import hgsc_cancer_tissue
-        >>> ax = hgsc_cancer_tissue().plot(column="class_name")
+        >>> from histolytics.data import hgsc_cancer_nuclei
+        >>> ax = hgsc_cancer_nuclei().plot(column="class_name")
         >>> ax.set_axis_off()
     ![out](../../img/hgsc_crop_nuc.png)
     """
     return _load(BASE_PATH / "hgsc_nest.parquet")
 
 
-def hgsc_stroma_nuclei():
-    """A GeoDataframe of segmented nuclei of a HGSC stroma.
+def hgsc_cancer_inst_mask():
+    """An instance raster mask (1500x1500px) of segmented nuclei of a HGSC tumor nest.
+
+    Note:
+        Pairs with:
+
+        - `hgsc_cancer_nuclei()`
+        - `hgsc_cancer_he()`
+        - `hgsc_cancer_type_mask()`
 
     Examples:
-        >>> from histolytics.data import hgsc_stroma_nuclei
-        >>> ax = hgsc_stroma_nuclei().plot(column="class_name")
+        >>> import matplotlib.pyplot as plt
+        >>> from histolytics.data import hgsc_cancer_type_mask
+        >>> from skimage.measure import label
+        >>> from skimage.color import label2rgb
+        >>> fig, ax = plt.subplots(figsize=(4, 4))
+        >>> im = hgsc_cancer_type_mask()
+        >>> ax.imshow(label2rgb(label(im), bg_label=0))
         >>> ax.set_axis_off()
-    ![out](../../img/hgsc_stroma_crop_nuc.png)
+    ![out](../../img/hgsc_cancer_inst.png)
     """
-    return _load(BASE_PATH / "hgsc_stromal_cells.parquet")
+    data = np.load(BASE_PATH / "hgsc_nest_inst_mask.npz")
+    return data["nuc_raster"]
+
+
+def hgsc_cancer_type_mask():
+    """An semantic raster mask (1500x1500px) of segmented nuclei of a HGSC tumor nest.
+
+    Note:
+        Pairs with:
+
+        - `hgsc_cancer_nuclei()`
+        - `hgsc_cancer_he()`
+        - `hgsc_cancer_inst_mask()`
+
+    Examples:
+        >>> import matplotlib.pyplot as plt
+        >>> from histolytics.data import hgsc_cancer_type_mask
+        >>> from skimage.color import label2rgb
+        >>> fig, ax = plt.subplots(figsize=(4, 4))
+        >>> im = hgsc_cancer_type_mask()
+        >>> ax.imshow(label2rgb(im, bg_label=0))
+        >>> ax.set_axis_off()
+    ![out](../../img/hgsc_cancer_type.png)
+    """
+    data = np.load(BASE_PATH / "hgsc_nest_type_mask.npz")
+    return data["nuc_raster"]
 
 
 def hgsc_cancer_he():
-    """A 1500x1500 H&E image of HGSC containing a tumor nest. Pairs with `hgsc_cancer_nuclei`.
+    """A 1500x1500 H&E image of HGSC containing a tumor nest.
+
+    Note:
+        Pairs with:
+
+        - `hgsc_cancer_nuclei()`
+        - `hgsc_cancer_inst_mask()`
+        - `hgsc_cancer_type_mask()`
 
     Examples:
         >>> import matplotlib.pyplot as plt
@@ -146,8 +217,26 @@ def hgsc_cancer_he():
     return FileHandler.read_img(BASE_PATH / "hgsc_nest.png")
 
 
+def hgsc_stroma_nuclei():
+    """A GeoDataframe of segmented nuclei of a HGSC stroma.
+
+    Note:
+        Pairs with `hgsc_stroma_he()`.
+
+    Examples:
+        >>> from histolytics.data import hgsc_stroma_nuclei
+        >>> ax = hgsc_stroma_nuclei().plot(column="class_name")
+        >>> ax.set_axis_off()
+    ![out](../../img/hgsc_stroma_crop_nuc.png)
+    """
+    return _load(BASE_PATH / "hgsc_stromal_cells.parquet")
+
+
 def hgsc_stroma_he():
-    """A 1500x1500 H&E image of HGSC containing stroma. Pairs with `hgsc_stroma_nuclei`.
+    """A 1500x1500 H&E image of HGSC containing stroma.
+
+    Note:
+        Pairs with `hgsc_stroma_nuclei()`.
 
     Examples:
         >>> import matplotlib.pyplot as plt
